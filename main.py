4cfea1b1937572ca7f9b569ba11b8f9138a4a075
Farhan-ds7 <farhanaman558@gmail.com>
2025-06-28 00:38:22 +0530

    added finding the angle and updated

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
 import cv2
 from pose_module import PoseDetector
 
-cap = cv2.VideoCapture(0)
-detector = PoseDetector()
-count=0
-dir=0
-
+cap=cv2.VideoCapture(0)
+detector=PoseDetector()
 while True:
     success, img = cap.read()
-    img = detector.findPose(img)
-    lmList = detector.getLandmarkPositions(img)
-
-    if len(lmList)!=0:
-        angle=detector.findAngle(img,lmList[11],lmList[13],lmList[15])
-        if angle>160:
-            if dir==1:
-                count+=1
-                dir=0
-        if angle<50:
-            if dir==0:
-                dir=1
-        cv2.putText(img,f'Reps:{count}',(20,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),3)
-        
-        print("Elbow Angle:", angle)
-
-    cv2.imshow("Pose Detection", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    img=detector.findPose(img)
+    lmList=detector.getLandmarkPositions(img)
+    img=detector.countBicepCurls(img,lmList)        
+    cv2.imshow("Bicep Curl Counter - Both Arms",img)
+    if cv2.waitKey(1)&0xFF==ord('q'):
         break
-
 cap.release()
 cv2.destroyAllWindows()
--- pose_module.py
+++ pose_module.py
@@ -5,9 +5,14 @@ import math
 
 class PoseDetector:
     def __init__(self):
-        self.mpDraw = mp.solutions.drawing_utils
-        self.mpPose = mp.solutions.pose
-        self.pose = self.mpPose.Pose()
+        self.mpDraw=mp.solutions.drawing_utils
+        self.mpPose=mp.solutions.pose
+        self.pose=self.mpPose.Pose()
+
+        self.count_right=0
+        self.dir_right=0
+        self.count_left=0
+        self.dir_left=0
 
     def findPose(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -17,13 +22,13 @@ class PoseDetector:
                 img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
         return img
 
-    def getLandmarkPositions(self, img):
-        lmList = []
+    def getLandmarkPositions(self,img):
+        lmList=[]
         if self.results.pose_landmarks:
-            for id, lm in enumerate(self.results.pose_landmarks.landmark):
-                h, w, _ = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                lmList.append((id, cx, cy))
+            for id,lm in enumerate(self.results.pose_landmarks.landmark):
+                h,w,_= img.shape
+                cx,cy=int(lm.x * w),int(lm.y*h)
+                lmList.append((id,cx,cy))
         return lmList
     
     def findAngle(self,img,p1,p2,p3,draw=True):
@@ -38,7 +43,35 @@ class PoseDetector:
             angle=360-angle
         
         cv2.circle(img,(x1,y1),10,(255,0,0),cv2.FILLED)
+        cv2.circle(img,(x2, y2),10,(255,0,0),cv2.FILLED)
+        cv2.circle(img,(x3, y3),10,(255,0,0),cv2.FILLED)
         cv2.line(img,(x1,y1),(x2,y2),(255,255,255),2)
-        cv2.putText(img,str(int(angle)),(x2-50,y2-20),cv2.FONT_HERSHEY_PLAIN,2,(0,255,0),2)
+        cv2.line(img,(x3,y3),(x2,y2),(255, 255, 255), 2)
+        cv2.putText(img,f'{int(angle)}',(x2 - 50, y2 + 40),
+                        cv2.FONT_HERSHEY_PLAIN,2,(0, 255, 0), 2)
 
         return angle
+    
+    def countBicepCurls(self,img,lmList):
+        if len(lmList)!=0:
+# right hand
+            angle_right=self.findAngle(img, lmList[12], lmList[14], lmList[16])
+            if angle_right>160:
+                if self.dir_right==1:
+                    self.count_right+=1
+                    self.dir_right=0
+            if angle_right<50:
+                if self.dir_right==0:
+                    self.dir_right=1
+# left hand
+            angle_left=self.findAngle(img,lmList[11],lmList[13],lmList[15])
+            if angle_left>160:
+                if self.dir_left==1:
+                    self.count_left+=1
+                    self.dir_left=0
+            if angle_left<50:
+                if self.dir_left==0:
+                   self.dir_left=1
+            cv2.putText(img,f'Right Reps:{self.count_right}',(20,50),cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),3)
+            cv2.putText(img,f'Left Reps:{self.count_left}',(20,90),cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),3)
+        return img
