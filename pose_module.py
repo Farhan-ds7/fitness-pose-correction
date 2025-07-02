265941f6859faace8fd4433d67816cbce4cb3071
Farhan-ds7 <farhanaman558@gmail.com>
2025-07-02 20:56:51 +0530

    Update pose_module.py

--- pose_module.py
+++ pose_module.py
@@ -45,27 +45,30 @@ class PoseDetector:
         cv2.circle(img,(x1,y1),10,(255,0,0),cv2.FILLED)
         cv2.circle(img,(x2, y2),10,(255,0,0),cv2.FILLED)
         cv2.circle(img,(x3, y3),10,(255,0,0),cv2.FILLED)
-        cv2.line(img,(x1,y1),(x2,y2),(255,255,255),2)
-        cv2.line(img,(x3,y3),(x2,y2),(255, 255, 255), 2)
+        cv2.line(img,(x1,y1),(x2,y2),(255,255,255),3)
+        cv2.line(img,(x3,y3),(x2,y2),(255, 255, 255),3)
         cv2.putText(img,f'{int(angle)}',(x2 - 50, y2 + 40),
                         cv2.FONT_HERSHEY_PLAIN,2,(0, 255, 0), 2)
         return angle
     
     def countBicepCurls(self,img,lmList):
-        if not lmList or len(lmList)<17:
+        if not lmList or len(lmList)<2:
             return img
         
-        right_shoulder_y=lmList[12][2]
-        left_shoulder_y=lmList[11][2]
-        shoulder_diff=abs(right_shoulder_y-left_shoulder_y)
-        shoulders_straight= shoulder_diff < 30
-        cv2.putText(img, f'Right Reps: {self.count_right}', (20, 50),cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
-        cv2.putText(img, f'Left Reps: {self.count_left}', (20, 90),cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+        left_shoulder_angle=self.findAngle(img,lmList[13],lmList[11],lmList[23],draw=True)  # Elbow-Shoulder-Hip
+        right_shoulder_angle=self.findAngle(img,lmList[14],lmList[12],lmList[24],draw=True)  # Elbow-Shoulder-Hip
+        shoulders_straight=(0<=left_shoulder_angle<=25)and(0<=right_shoulder_angle<=25)
+
+        cv2.putText(img,f'Right Reps:{self.count_right}',(20,50),
+                cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),3)
+        cv2.putText(img,f'Left Reps:{self.count_left}',(20,90),
+                cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),3)
+
         if not shoulders_straight:
                 cv2.putText(img, "Keep Shoulders Straight!!", (20, 130),cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 3)
                 return img
         
-        angle_right = self.findAngle(img, lmList[12], lmList[14], lmList[16])        
+        angle_right = self.findAngle(img,lmList[12],lmList[14],lmList[16])        
         if angle_right>160:
             if self.dir_right==1:
                 self.count_right+=1
@@ -74,7 +77,7 @@ class PoseDetector:
             if self.dir_right==0:
                 self.dir_right=1
 
-        angle_left = self.findAngle(img, lmList[11], lmList[13], lmList[15])
+        angle_left = self.findAngle(img,lmList[11],lmList[13],lmList[15])
         if angle_left>160:
             if self.dir_left==1:
                 self.count_left+=1
